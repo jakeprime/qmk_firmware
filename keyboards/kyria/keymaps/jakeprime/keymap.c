8dafa2db92ec93bed9a79dc848218506467c6299
jakeprime <jake.prime@gmail.com>
2020-12-29 14:54:49 +0000

    Tidy up QWERTY layer code spacing

--- keyboards/kyria/keymaps/jakeprime/keymap.c
+++ keyboards/kyria/keymaps/jakeprime/keymap.c
@@ -33,14 +33,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |LShift|LShift|  |LShift|LShift|   N  |   M  | ,  < | . >  | /  ? | RShift |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        | Ctl  | Alt  |_RAISE| Cmd  |      |  | Enter| Space|_LOWER| Bksp |   `  |
+ *                        | Ctl  | Alt  |_RAISE| Cmd  |      |  | Enter|_LOWER| Space| Bksp |   `  |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                                          KC_Y, KC_U, KC_I, KC_O, KC_P, KC_ENT,
-        MT(MOD_LCTL, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G,                                            KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_RSFT,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LSFT, KC_LSFT,                                       KC_LSFT, KC_LSFT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                 KC_LCTL, KC_LALT, MO(_RAISE), KC_LGUI, _______,                          KC_ENT, KC_SPC, MO(_LOWER), KC_BSPC, KC_GRV
+                      KC_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
+        MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RSFT,
+                     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_LSFT, KC_LSFT, KC_LSFT, KC_LSFT,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                                KC_LCTL, KC_LALT, MO(_RAISE), KC_LGUI, _______, KC_ENT,  MO(_LOWER), KC_SPC,  KC_BSPC, KC_GRV
     ),
 /*
  * Lower Layer: Symbols
