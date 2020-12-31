5f83769b2747324318ae696ef78b9a6fe77a6269
jakeprime <jake.prime@gmail.com>
2020-12-31 11:24:57 +0000

    Add Delete key

--- keyboards/kyria/keymaps/jakeprime/keymap.c
+++ keyboards/kyria/keymaps/jakeprime/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.                              ,-------------------------------------------.
  * |   Tab  |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  | Enter  |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |  Esc   |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |        |
+ * |  Esc   |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : | Delete |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * |Sft/Caps|   Z  |   X  |   C  |   V  |   B  |CmdAlt|SftCmd|  |LShift|LShift|   N  |   M  | ,  < | . >  | /  ? |Sft/Caps|
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_QWERTY] = LAYOUT(
         KC_TAB,          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                KC_Y,    KC_U,      KC_I,    KC_O,    KC_P,    KC_ENT,
-        KC_ESC,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                KC_H,    KC_J,      KC_K,    KC_L,    KC_SCLN, _______,
+        KC_ESC,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                KC_H,    KC_J,      KC_K,    KC_L,    KC_SCLN, KC_DEL,
         LSFT_T(KC_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      A(KC_LCMD), S(KC_LCMD), KC_LSFT, KC_LSFT, KC_N,    KC_M,      KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_CAPS),
                                            KC_LCTL, KC_LALT, MO(_SYMB), KC_LCMD,    C(KC_LALT), KC_ENT,  KC_SPC,  KC_BSPC, MO(_NUMS), KC_GRV
     ),
