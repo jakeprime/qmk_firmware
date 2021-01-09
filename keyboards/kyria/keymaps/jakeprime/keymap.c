11a63ce679707a3450345bde2b41792d09ece5ab
jakeprime <jake.prime@gmail.com>
2021-01-09 10:39:04 +0000

    Remove caps lock from shift buttons, too many issues

--- keyboards/kyria/keymaps/jakeprime/keymap.c
+++ keyboards/kyria/keymaps/jakeprime/keymap.c
@@ -31,16 +31,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |  Esc   |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : | Delete |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |Sft/Caps|   Z  |   X  |   C  |   V  |   B  |CmdAlt|SftCmd|  |LShift|LShift|   N  |   M  | ,  < | . >  | /  ? |Sft/Caps|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |CmdAlt|SftCmd|  |LShift| Caps |   N  |   M  | ,  < | . >  | /  ? | RShift |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        | Ctl  | Alt  |_SYMB | Cmd  |CtlAlt|  | Enter| Space|_NUMS | Bksp |   `  |
+ *                        | Ctl  | Alt  |_SYMB | Cmd  |CtlAlt|  | Enter|_NUMS |Space | Bksp |   `  |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-        KC_TAB,          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                KC_Y,      KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
-        KC_ESC,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                KC_H,      KC_J,    KC_K,    KC_L,    KC_SCLN, KC_DEL,
-        LSFT_T(KC_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      A(KC_LCMD), S(KC_LCMD), KC_LSFT, KC_LSFT, KC_N,      KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_CAPS),
-                                           KC_LCTL, KC_LALT, MO(_SYMB), KC_LCMD,    C(KC_LALT), KC_ENT,  KC_SPC,  MO(_NUMS), KC_BSPC, KC_GRV
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_DEL,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      A(KC_LCMD), S(KC_LCMD), KC_LSFT, KC_CAPS,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KS_RSFT,
+                                   KC_LCTL, KC_LALT, MO(_SYMB), KC_LCMD,    C(KC_LALT), KC_ENT,  MO(_NUMS), KC_SPC,  KC_BSPC, KC_GRV
     ),
 /*
  * Lower Layer: Symbols
