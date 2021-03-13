23cbb0850fb22d007da9cd88c2a45e13d62bf634
jakeprime <jake.prime@gmail.com>
2021-03-13 12:35:38 +0000

    Use middle fingers for shift

--- keyboards/kyria/keymaps/jakeprime/config.h
+++ keyboards/kyria/keymaps/jakeprime/config.h
@@ -31,3 +31,6 @@
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
 #define SPLIT_USB_DETECT
 #define NO_USB_STARTUP_CHECK
+
+// https://docs.qmk.fm/#/tap_hold?id=ignore-mod-tap-interrupt
+#define IGNORE_MOD_TAP_INTERRUPT
