4c41f9ec683299896a8300b164199d22ca01f0b1
jakeprime <jake.prime@gmail.com>
2021-04-28 22:34:22 +0100

    Home row mods for symbols too now

--- keyboards/kyria/keymaps/jakeprime/keymap.c
+++ keyboards/kyria/keymaps/jakeprime/keymap.c
@@ -69,21 +69,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.                              ,-------------------------------------------.
  * |        |  +   |  ^   |  {   |  }   |  #   |                              |   \  |  '   |  "   |  `   |  @   |        |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        |  &   |  |   |  (   |  )   |  $   |                              |   ~  |  _   |  -   |  /   |  :   |        |
+ * |        |  &   |  |   |  (   |  )   |  $   |                              |   ~  |  _   |  -   |  +   |  :   |        |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * |        |  %   |  *   |  [   |  ]   |  £   |      |      |  |      |      |   !  |  =   |  <   |  >   |  ?   |        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        |      |      |      |      |      |  |      |      | _RGB |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
-/*
-To press a mod with a symbol the home mod must be held first, and then the symbol layer change, and then the symbol.
-To allow home row mods and layer change to pressed in any order it will need something like this:
-https://discord.com/channels/440868230475677696/473506116718952450/828578189957398588
-*/
+#define H_AMPR LSFT_T(KC_AMPR)
+#define H_PIPE LCTL_T(KC_PIPE)
+#define H_LPRN LALT_T(KC_LPRN)
+#define H_RPRN LGUI_T(KC_RPRN)
+
+#define H_UNDS RGUI_T(KC_UNDS)
+#define H_MINS LALT_T(KC_MINS)
+#define H_PLUS RCTL_T(KC_PLUS)
+#define H_COLN RSFT_T(KC_COLN)
+
     [_SYMB] = LAYOUT(
       XXXXXXX, KC_PLUS, S(KC_6), KC_LCBR, KC_RCBR, A(KC_3),                                     KC_BSLS, KC_QUOT, KC_DQUO, KC_GRV,  KC_AT,   XXXXXXX,
-      XXXXXXX, KC_AMPR, KC_PIPE, KC_LPRN, KC_RPRN, KC_DLR,                                      KC_TILD, KC_UNDS, KC_MINS, KC_SLSH, KC_COLN, XXXXXXX,
+      XXXXXXX, H_AMPR,  H_PIPE,  H_LPRN,  H_RPRN,  KC_DLR,                                      KC_TILD, H_UNDS,  H_MINS,  H_PLUS,  H_COLN,  XXXXXXX,
       XXXXXXX, KC_PERC, KC_ASTR, KC_LBRC, KC_RBRC, S(KC_3), _______, _______, _______, _______, KC_EXLM, KC_EQL,  KC_LT,   KC_GT,   KC_QUES, XXXXXXX,
                                  _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX
     ),
@@ -140,10 +145,10 @@ https://discord.com/channels/440868230475677696/473506116718952450/8285781899573
  *                        `----------------------------------'  `----------------------------------'
  */
     [_NUMS] = LAYOUT(
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     KC_PSLS, KC_P7, KC_P8, KC_P9, KC_PPLS, XXXXXXX,
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     KC_PAST, KC_P4, KC_P5, KC_P6, XXXXXXX, XXXXXXX,
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PMNS, KC_P1, KC_P2, KC_P3, XXXXXXX, XXXXXXX,
-                                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, KC_P0,   KC_PDOT, XXXXXXX
+      _______, _______, _______, _______, _______, _______,                                     KC_PSLS, KC_P7, KC_P8, KC_P9, KC_PPLS, _______,
+      _______, _______, _______, _______, _______, _______,                                     KC_PAST, KC_P4, KC_P5, KC_P6, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PMNS, KC_P1, KC_P2, KC_P3, _______, _______,
+                                 _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, _______
     )
 // /*
 //  * Layer template
@@ -169,6 +174,42 @@ https://discord.com/channels/440868230475677696/473506116718952450/8285781899573
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _SYMB, _NAV, _RGB);
+}
+
+bool process_key_tap(uint16_t keycode, keyrecord_t *record) {
+    if (record->tap.count > 0) {
+        // tap
+        if (record->event.pressed) {
+            register_code16(keycode);
+        } else {
+            unregister_code16(keycode);
+        }
+        return false; // disable the default action
+    }
+    // hold - use the default action
+    return true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch(keycode){
+        case H_AMPR:
+            return process_key_tap(KC_AMPR, record);
+        case H_PIPE:
+            return process_key_tap(KC_PIPE, record);
+        case H_LPRN:
+            return process_key_tap(KC_LPRN, record);
+        case H_RPRN:
+            return process_key_tap(KC_RPRN, record);
+
+        case H_UNDS:
+            return process_key_tap(KC_UNDS, record);
+        case H_PLUS:
+            return process_key_tap(KC_PLUS, record);
+        case H_COLN:
+            return process_key_tap(KC_COLN, record);
+        default:
+            return true;
+    }
 }
 
 #ifdef OLED_DRIVER_ENABLE
