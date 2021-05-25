f026179c273c7f575cbb6d084f9bc954373f7767
jakeprime <jake.prime@gmail.com>
2021-05-25 15:47:56 +0100

    Swap TAB and DEL
    Two reasons:
    
    - I alt-tab more than I del
    - At some point I will want to be able to hold del to repeat, probably using tap dance, and this will be less of an issue with the auxiliaryu
    CMD than the SYM mod-tap

--- keyboards/kyria/keymaps/jakeprime/keymap.c
+++ keyboards/kyria/keymaps/jakeprime/keymap.c
@@ -55,14 +55,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #define ESC_MED LT(_MEDIA, KC_ESC)
 #define SPC_NAV LT(_NAV, KC_SPC)
-#define DEL_SYM LT(_SYMB, KC_DEL)
-#define CMD_TAB LGUI_T(KC_TAB)
+#define TAB_SYM LT(_SYMB, KC_TAB)
+#define DEL_CMD LGUI_T(KC_DEL)
 
     [_QWERTY] = LAYOUT(
         XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX,
         XXXXXXX, QWRT_A,  QWRT_S,  QWRT_D,  QWRT_F,  KC_G,                                        KC_H,    QWRT_J,  QWRT_K,  QWRT_L,  QWRT_SC, XXXXXXX,
         XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
-                                   XXXXXXX, ESC_MED, DEL_SYM, CMD_TAB, HALMAK,  KC_CAPS, KC_ENT,  SPC_NAV, KC_BSPC, XXXXXXX
+                                   XXXXXXX, ESC_MED, TAB_SYM, DEL_CMD, HALMAK,  KC_CAPS, KC_ENT,  SPC_NAV, KC_BSPC, XXXXXXX
     ),
 
 /*
