9c7556f513c82807d1cf102a104a2a35652e2bfe
jakeprime <jake.prime@gmail.com>
2022-01-07 16:16:38 +0000

    Fix font++ to use `=` instead of `+`

--- keyboards/kyria/keymaps/jakeprime/keymap.c
+++ keyboards/kyria/keymaps/jakeprime/keymap.c
@@ -489,11 +489,11 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     }
     else if (index == 1) {
 #ifdef JP_LIGHT_SIDE
-        if (clockwise) tap_code16(G(KC_PLUS));
+        if (clockwise) tap_code16(G(KC_EQL));
         else tap_code16(G(KC_MINS));
 #else
         if (clockwise) tap_code16(G(KC_MINS));
-        else tap_code16(G(KC_PLUS));
+        else tap_code16(G(KC_EQL));
 #endif
     }
     return false;
